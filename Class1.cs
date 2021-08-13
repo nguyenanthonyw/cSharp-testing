f158997c20d39da6be1cc089b56e9e14141a992c
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-08-12 20:11:29 -0500

    test

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Class1
+{
+
+	static void Main(String[] args)
+    {
+		Console.Write("Hello");
+	}
+
+}
---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Main
+{
+
+	static void Main(String[] args)
+    {
+		Console.Write("Hello");
+	}
+
+}
